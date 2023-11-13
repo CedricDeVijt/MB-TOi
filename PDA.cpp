f1995e53d0dea765af930750be9518923ce083e8
CedricDeVijt <cedric.devijt@gmail.com>
2023-11-13 12:01:51 +0100

    completed implementation of toCFG() function

--- PDA.cpp
+++ PDA.cpp
@@ -1,40 +1,29 @@
-#include <iostream>
 #include <fstream>
+#include <cmath>
+#include <string>
+#include <vector>
 #include "PDA.h"
 #include "json.hpp"
 
 using namespace std;
 using json = nlohmann::json;
 
-bool comparePairWords2(const pair<string,vector<string>>& a, const pair<string,vector<string>>& b) {
-    if (a.first == b.first) {
-        string one;
-        for (const auto& elem: a.second)
-            one += elem;
-        string two;
-        for (const auto& elem: b.second)
-            two += elem;
-        return one < two;
-    }
-    return a.first < b.first;
-}
-
-PDA::PDA(const std::string&filename) {
+PDA::PDA(const std::string &filename) {
     ifstream input_stream(filename);
     json json_input = json::parse(input_stream);
 
     // Get States
-    for (const auto&symbol: json_input["States"]) {
+    for (const auto &symbol: json_input["States"]) {
         states.push_back(symbol);
     }
 
     // Get Alphabet
-    for (const auto&symbol: json_input["Alphabet"]) {
+    for (const auto &symbol: json_input["Alphabet"]) {
         alphabet.push_back(symbol);
     }
 
     // Get StackAlphabet
-    for (const auto&symbol: json_input["StackAlphabet"]) {
+    for (const auto &symbol: json_input["StackAlphabet"]) {
         stackAlphabet.push_back(symbol);
     }
 
@@ -45,7 +34,7 @@ PDA::PDA(const std::string&filename) {
     startStack = std::string(json_input["StartStack"]);
 
     // Get Transitions
-    for (const auto&transition: json_input["Transitions"]) {
+    for (const auto &transition: json_input["Transitions"]) {
         std::string from_state = transition["from"];
         std::string input_symbol = transition["input"];
         std::string stack_top = transition["stacktop"];
@@ -53,31 +42,66 @@ PDA::PDA(const std::string&filename) {
         std::vector<std::string> replacement = transition["replacement"];
 
         // Create a Transition tuple and add it to the transitions vector
-        transitions.push_back(std::make_tuple(from_state, input_symbol[0], stack_top, to_state, replacement));
+        transitions.emplace_back(from_state, input_symbol[0], stack_top, to_state, replacement);
     }
 }
 
-CFG PDA::toCFG() {
-    // Get Variables
+std::vector<std::string> PDA::getCFGVariables() {
     std::vector<std::string> variables = {"S"};
-    for (const auto&state1: states) {
-        for (const auto&symbol: stackAlphabet) {
-            for (const auto&state2: states) {
+    for (const auto &state1: states) {
+        for (const auto &symbol: stackAlphabet) {
+            for (const auto &state2: states) {
                 variables.push_back("[" + state1 + "," + string(symbol) + "," + string(state2) + "]");
             }
         }
     }
-
-    // Get Terminals
-    std::vector<std::string> terminals = alphabet;
-
-    // Get Productions
-    std::vector<std::pair<std::string, std::vector<std::string>>> productions;
-    
-
-
-    //  Get Start
-    std::string start = "S";
-
-    return {variables, terminals, productions, start};
+    return variables;
 }
+
+std::vector<std::pair<std::string, std::vector<std::string>>> PDA::getCFGProductions() {
+    std::vector<std::pair<std::string, std::vector<std::string>>> productions;
+
+    // Start state
+    productions.reserve(states.size());
+    for (auto &state: states) {
+        productions.emplace_back("S",
+                                 std::vector<std::string>{"[" + startState + "," + startStack + "," + state + "]"});
+    }
+
+    // Transitions
+    for (const auto &transition: transitions) {
+        std::string from_state = std::get<0>(transition);
+        char input_char = std::get<1>(transition);
+        string input_symbol = string(1, std::get<1>(transition));
+        if (input_char == '\0'){
+            input_symbol = "";
+        }
+        std::string stack_top = std::get<2>(transition);
+        std::string to_state = std::get<3>(transition);
+        std::vector<std::string> replacement = std::get<4>(transition);
+
+        if (replacement.empty()){
+            string head = "[" + from_state + "," + stack_top + "," + to_state + "]";
+            productions.emplace_back(head, std::vector<std::string>{input_symbol});
+        } else if (replacement.size() == 1){
+            for (auto state1: states){
+                string head = "[" + from_state + "," + stack_top + "," + state1 + "]";
+                productions.emplace_back(head, std::vector<std::string>{input_symbol + " [" + to_state + "," + replacement[0] + "," + state1 + "]"});
+            }
+        } else if (replacement.size() == 2){
+            for (auto state1: states){
+                for (auto state2: states){
+                    string head = "[" + from_state + "," + stack_top + "," + state1 + "]";
+                    productions.emplace_back(head, std::vector<std::string>{input_symbol + " [" + to_state + "," + replacement[0] + "," + state2 + "]"+ " [" + state2 + "," + replacement[1] + "," + state1 + "]"});
+                }
+            }
+        }
+
+    }
+    return productions;
+};
+
+CFG PDA::toCFG() {
+    return {getCFGVariables(), alphabet, getCFGProductions(), "S"};
+}
+
--- PDA.h
+++ PDA.h
@@ -1,6 +1,7 @@
 #ifndef TOI_PDA_H
 #define TOI_PDA_H
 #include <string>
+#include <tuple>
 #include "CFG.h"
 
 typedef std::tuple<std::string, char, std::string, std::string, std::vector<std::string>> Transition;
@@ -15,8 +16,11 @@ class PDA {
 
 public:
     explicit PDA(const std::string&filename);
-
     CFG toCFG();
+
+private:
+    std::vector<std::string> getCFGVariables();
+    std::vector<std::pair<std::string, std::vector<std::string>>> getCFGProductions();
 };
 
 
